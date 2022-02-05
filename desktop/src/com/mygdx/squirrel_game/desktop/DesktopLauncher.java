bab7eb74a189676b817c6061d0b416b43c3a4954
Ilan Joselevich <personal@ilanjoselevich.com>
2022-02-05 21:32:00 +0200

    desktop: lwjgl -> lwjgl3

--- desktop/src/com/mygdx/squirrel_game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/squirrel_game/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package com.mygdx.squirrel_game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.squirrel_game.squirrel_game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Squirrel Game";
-		config.width = 1280;
-		config.height = 800;
-		new LwjglApplication(new squirrel_game(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280, 800);
+		config.setTitle("Squirrel Game");
+		new Lwjgl3Application(new squirrel_game(), config);
 	}
 }
