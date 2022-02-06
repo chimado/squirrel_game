f09b54d95533df93006c90e2713aa00b73508982
chimado <47482657+chimado@users.noreply.github.com>
2022-02-06 10:32:45 +0200

    Revert "lwjgl3, nix, docs, gradlew"

--- desktop/src/com/mygdx/squirrel_game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/squirrel_game/desktop/DesktopLauncher.java
@@ -1,14 +1,15 @@
 package com.mygdx.squirrel_game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.squirrel_game.squirrel_game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(1280, 800);
-		config.setTitle("Squirrel Game");
-		new Lwjgl3Application(new squirrel_game(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Squirrel Game";
+		config.width = 1280;
+		config.height = 800;
+		new LwjglApplication(new squirrel_game(), config);
 	}
 }
