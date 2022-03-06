21c42986d9d6a9666e0ac68c7a1524f304006bad
chimado <ido.ludmer@gmail.com>
2022-03-06 09:15:58 +0200

    added clicking on squirrel to play idle animation to main menu screen

--- core/src/com/mygdx/squirrel_game/UserInterfaceScreen.java
+++ core/src/com/mygdx/squirrel_game/UserInterfaceScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -14,13 +15,15 @@ import static com.mygdx.squirrel_game.game_screen.worldWidth;
 import static com.mygdx.squirrel_game.game_screen.worldHeight;
 // import com.badlogic.gdx.audio.Music; reserved for later use
 // import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class UserInterfaceScreen implements Screen{
     final squirrel_game game;
 
+    static final int playerStartingX = 200, playerStartingY = 200;
+    squirrel player;
+    Array<Platform> platforms; // stores the platforms for the main menu screen
     OrthographicCamera camera;
     Viewport viewport;
     float deltaTime;
@@ -32,6 +35,8 @@ public class UserInterfaceScreen implements Screen{
     public UserInterfaceScreen(final squirrel_game game){
         this.game = game;
         deltaTime = 0;
+        player = new squirrel(playerStartingX, playerStartingY);
+        platforms = new Array<Platform>();
 
         // create the camera and the viewport
         camera = new OrthographicCamera();
@@ -51,7 +56,13 @@ public class UserInterfaceScreen implements Screen{
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 1, 0.5f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         deltaTime = Gdx.graphics.getDeltaTime();
+        if (mouse.overlaps(player.bounds) && Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
+            deltaTime += 2f;
+        else if (player.idle_animation_time <= 2f){
+            deltaTime = 0;
+        }
 
         // tell the camera to update its matrices.
         camera.update();
@@ -68,16 +79,38 @@ public class UserInterfaceScreen implements Screen{
             shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
             shapeRenderer.setColor(Color.CYAN);
             shapeRenderer.rect(mouse.x, mouse.y, mouse.width, mouse.height);
+            shapeRenderer.rect(player.bounds.x, player.bounds.y, player.bounds.width, player.bounds.height);
+
+            for (Platform platform : platforms) {
+                shapeRenderer.rect(platform.bounds.x, platform.bounds.y, platform.bounds.width, platform.bounds.height);
+                if (platform.hasDirt) shapeRenderer.rect(platform.getDirt().bounds.x, platform.getDirt().bounds.y,
+                        platform.getDirt().bounds.width, platform.getDirt().bounds.height);
+            }
+
+            shapeRenderer.end();
         }
 
+        // Render objects for background
         game.batch.begin();
 
+        game.batch.draw(player.render(deltaTime), player.x, player.y, player.width, player.height);
+
+        for (Platform platform : platforms) {
+            game.batch.draw(platform.getPlatformTexture(), platform.x, platform.y, platform.width, platform.height);
+            if (platform.hasDirt) game.batch.draw(platform.getDirt().getDirtTexture(), platform.getDirt().x,
+                    platform.getDirt().y, platform.getDirt().width, platform.getDirt().height);
+        }
+
         game.batch.end();
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) shapeRenderer.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)){
+            dispose();
+            Gdx.app.exit();
+        }
     }
 
     // updates the mouse's position according to the computers mouse
-    public void updateMouse(){
+    public void updateMouse() {
         mousePosition.set(Gdx.input.getX(), Gdx.input.getY(), 0);
         camera.unproject(mousePosition);
         mouse.x = mousePosition.x - mouse.width / 2;
